a98bb3dfe88425a2efa48133f5a39296c8a0a28f
viktorKorneev <korneevviktor90@yandex.ru>
2026-03-13 19:57:07 +0300

    step-3

--- battleship/main.js
+++ battleship/main.js
@@ -1,12 +1,16 @@
+//позиция корабля
 const location1 = 3;
 const location2 = 4;
 const location3 = 5;
-
+//потоплен или нет
 let isSunk = false;
+//предполагаемая позиция корабля ("выстрел")
 let guess;
 
 while (isSunk === false) {
+    //не забываем, что строку надо превратить в число
     guess = +prompt("Fire! Enter a number 0-6")
+    //логическое "или"
     if(guess === location1 || guess === location2 || guess === location3) {
         alert("HIT!")
     } else {
