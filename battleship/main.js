9621cb624a3a43aa197bae417dab18cc139e121e
viktorKorneev <korneevviktor90@yandex.ru>
2026-03-13 19:54:38 +0300

    step-2

--- battleship/main.js
+++ battleship/main.js
@@ -6,5 +6,10 @@ let isSunk = false;
 let guess;
 
 while (isSunk === false) {
-    
+    guess = +prompt("Fire! Enter a number 0-6")
+    if(guess === location1 || guess === location2 || guess === location3) {
+        alert("HIT!")
+    } else {
+        alert("MISS!")
+    }
 }
