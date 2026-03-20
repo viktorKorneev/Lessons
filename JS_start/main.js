c9c39dd89230057f6c737571f4861e157a0fad19
viktorKorneev <korneevviktor90@yandex.ru>
2026-03-20 19:29:44 +0300

    step-19

--- JS_start/main.js
+++ JS_start/main.js
@@ -1,10 +1,10 @@
 const depositAmount = 1000;
 const yearRate = 15;
 const depositTermInYears = 2;
+let count = 0;
 
 function getDepositIncome(amount, rate, term) {
   let depositIncome = 0;
-  let count = 0;
 
   while (count < term) {
     depositIncome = depositIncome + amount * (rate / 100);
@@ -12,15 +12,14 @@ function getDepositIncome(amount, rate, term) {
   }
   return depositIncome;
 }
-const depositIncome_1 = getDepositIncome(
-  depositAmount,
-  yearRate,
-  depositTermInYears,
-);
-const depositIncome_2 = getDepositIncome(1500, 16, 3);
-const totalIncome = depositIncome_1 + depositIncome_2;
+// const income_1 = getDepositIncome(depositAmount, yearRate, depositTermInYears);
+const income_2 = getDepositIncome(1000, 30, 3);
+console.log(income_2);
 
-console.log(totalIncome);
+// const depositIncome_2 = getDepositIncome(1500, 16, 3);
+// const totalIncome = depositIncome_1 + depositIncome_2;
+
+// console.log(totalIncome);
 
 // const inStock = true;
 // const onSale = false;
