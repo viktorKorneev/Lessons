1d84adbcffbce18024ba832305c5cdaf472fe502
viktorKorneev <korneevviktor90@yandex.ru>
2026-03-20 19:40:21 +0300

    step-20

--- JS_start/main.js
+++ JS_start/main.js
@@ -1,20 +1,27 @@
 const depositAmount = 1000;
 const yearRate = 15;
 const depositTermInYears = 2;
-let count = 0;
 
-function getDepositIncome(amount, rate, term) {
-  let depositIncome = 0;
+function wrapper() {
+  function getDepositIncome(amount, rate, term) {
+    let depositIncome = 0;
+    let count = 0;
 
-  while (count < term) {
-    depositIncome = depositIncome + amount * (rate / 100);
-    count = count + 1;
+    while (count < term) {
+      depositIncome = depositIncome + amount * (rate / 100);
+      count = count + 1;
+    }
+    return depositIncome;
   }
-  return depositIncome;
+  const income_1 = getDepositIncome(
+    depositAmount,
+    yearRate,
+    depositTermInYears,
+  );
+  console.log(income_1);
 }
-// const income_1 = getDepositIncome(depositAmount, yearRate, depositTermInYears);
-const income_2 = getDepositIncome(1000, 30, 3);
-console.log(income_2);
+wrapper()
+
 
 // const depositIncome_2 = getDepositIncome(1500, 16, 3);
 // const totalIncome = depositIncome_1 + depositIncome_2;
