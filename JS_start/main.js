20bb861613391cd17bc1d0e64acc88e497d54f32
viktorKorneev <korneevviktor90@yandex.ru>
2026-03-20 18:40:31 +0300

    step-12

--- JS_start/main.js
+++ JS_start/main.js
@@ -12,16 +12,40 @@
 //   document.write(count + ": " + sum + "<br>");
 // }
 
-const inStock = true;
-const onSale = false;
+// const inStock = true;
+// const onSale = false;
 
-const temp = 83;
-const willRain = true;
-const humid = willRain && temp > 80;
+// const temp = 83;
+// const willRain = true;
+// const humid = willRain && temp > 80;
 
-console.log(humid);
+// console.log(humid);
 
-// const byIt = onSale && inStock; // true?
+// // const byIt = onSale && inStock; // true?
 
-const buyIt = onSale && inStock; // false
-console.log(buyIt);
+// const buyIt = onSale && inStock; // false
+// console.log(buyIt);
+
+let dogName = "rover";
+let dogWeight = 23;
+if(dogWeight > 20) {
+    console.log(dogName + " says WOOF WOOF");
+} else {
+    console.log(dogName+ " says woof woof");
+}
+
+dogName = "spot";
+dogWeight = 13;
+if(dogWeight > 20) {
+    console.log(dogName + " says WOOF WOOF");
+} else {
+    console.log(dogName+ " says woof woof");
+}
+
+dogName = "spike";
+dogWeight = 53;
+if(dogWeight > 20) {
+    console.log(dogName + " says WOOF WOOF");
+} else {
+    console.log(dogName+ " says woof woof");
+}
