094ac75e4a9bc79015db560e025d62a887233c62
viktorKorneev <korneevviktor90@yandex.ru>
2026-03-20 18:41:44 +0300

    step-12

--- JS_start/main.js
+++ JS_start/main.js
@@ -49,3 +49,11 @@ if(dogWeight > 20) {
 } else {
     console.log(dogName+ " says woof woof");
 }
+
+dogName = "lady";
+dogWeight = 17;
+if(dogWeight > 20) {
+    console.log(dogName + " says WOOF WOOF");
+} else {
+    console.log(dogName+ " says woof woof");
+}
