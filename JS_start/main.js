6886d619408c726bf00224123ed51074e6358071
viktorKorneev <korneevviktor90@yandex.ru>
2026-03-20 18:51:48 +0300

    step-13

--- JS_start/main.js
+++ JS_start/main.js
@@ -26,34 +26,15 @@
 // const buyIt = onSale && inStock; // false
 // console.log(buyIt);
 
-let dogName = "rover";
-let dogWeight = 23;
-if(dogWeight > 20) {
-    console.log(dogName + " says WOOF WOOF");
-} else {
-    console.log(dogName+ " says woof woof");
+function bark(name, weight) {
+  if (weight > 20) {
+    console.log(name + " says WOOF WOOF");
+  } else {
+    console.log(name + " says woof woof");
+  }
 }
 
-dogName = "spot";
-dogWeight = 13;
-if(dogWeight > 20) {
-    console.log(dogName + " says WOOF WOOF");
-} else {
-    console.log(dogName+ " says woof woof");
-}
-
-dogName = "spike";
-dogWeight = 53;
-if(dogWeight > 20) {
-    console.log(dogName + " says WOOF WOOF");
-} else {
-    console.log(dogName+ " says woof woof");
-}
-
-dogName = "lady";
-dogWeight = 17;
-if(dogWeight > 20) {
-    console.log(dogName + " says WOOF WOOF");
-} else {
-    console.log(dogName+ " says woof woof");
-}
+bark("rover", 53);
+bark("spot", 13);
+bark("spike", 53);
+bark("lady", 17);
