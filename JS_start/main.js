de1193cf22554455d4a0c07a796514c7ed0793ce
viktorKorneev <korneevviktor90@yandex.ru>
2026-03-20 18:58:04 +0300

    step-14

--- JS_start/main.js
+++ JS_start/main.js
@@ -26,15 +26,23 @@
 // const buyIt = onSale && inStock; // false
 // console.log(buyIt);
 
-function bark(name, weight) {
-  if (weight > 20) {
-    console.log(name + " says WOOF WOOF");
-  } else {
-    console.log(name + " says woof woof");
-  }
-}
+// function bark(name, weight) {
+//   if (weight > 20) {
+//     console.log(name + " says WOOF WOOF");
+//   } else {
+//     console.log(name + " says woof woof");
+//   }
+// }
 
-bark("rover", 53);
-bark("spot", 13);
-bark("spike", 53);
-bark("lady", 17);
+// bark("rover", 53);
+// bark("spot", 13);
+// bark("spike", 53);
+// bark("lady", 17);
+
+function makeTea(cups, tea) {
+  console.log("Brewing " + cups + " cups of " + tea);
+}
+makeTea(3, "Earl Grey")
+// makeTea(3)
+// makeTea(3, "Earl Grey", "hey ma!", 42)
+// makeTea("Earl Grey", 3)
