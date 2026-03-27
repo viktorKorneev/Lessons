71c3a05b3d599d8e9f6ece7463fe376893c83984
viktorKorneev <korneevviktor90@yandex.ru>
2026-03-27 21:46:08 +0300

    step-27

--- JS_start/main.js
+++ JS_start/main.js
@@ -84,26 +84,17 @@
 // console.log(pets);
 // console.log(scores);
 
+function getRandomElement(array) {
+  const randomIndex = Math.floor(Math.random() * array.length);
+  return array[randomIndex];
+}
+
 function makePhrases() {
   const words1 = ["Мой кот", "Моя собака", "Мой попугай"];
   const words2 = ["любит есть", "хочет погрызть", "всегда ищет"];
   const words3 = ["морковку", "макароны", "косточку"];
 
-  const randomIndex_1 = Math.floor(Math.random() * words1.length);
-  const randomIndex_2 = Math.floor(Math.random() * words2.length);
-  const randomIndex_3 = Math.floor(Math.random() * words3.length);
-
-  const phrase =
-    words1[randomIndex_1] +
-    " " +
-    words2[randomIndex_2] +
-    " " +
-    words3[randomIndex_3];
-
-  return phrase;
+  return getRandomElement(words1) + " " + getRandomElement(words2) + " " + getRandomElement(words3);
 }
 
-
 alert(makePhrases());
-
-
