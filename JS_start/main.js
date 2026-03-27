cf1c8709e71ec859021ce9457ab56a86419602f0
viktorKorneev <korneevviktor90@yandex.ru>
2026-03-27 20:47:57 +0300

    step-22

--- JS_start/main.js
+++ JS_start/main.js
@@ -68,3 +68,18 @@
 // barkAtTheMoon();
 
 const scores = [60, 50, 60, 58, 54, 54, 58, 50, 52, 54];
+
+console.log(scores[0]);
+console.log(scores[2]);
+
+const pets = ["dog", "cat", "rat"];
+
+console.log(pets[2]);
+console.log(pets[3]);
+
+console.log(pets.length);
+console.log(scores.length);
+
+pets[2] = "parrot";
+console.log(pets);
+
