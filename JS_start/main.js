943e80699d2c443b16a19810d51d8a6e8e9d399e
viktorKorneev <korneevviktor90@yandex.ru>
2026-04-12 13:58:13 +0300

    step-38

--- JS_start/main.js
+++ JS_start/main.js
@@ -187,9 +187,17 @@ function printAndGetHighScore(array) {
 const car = {
   make: "Chevy",
   model: "Bel Air",
-  year: "1957",
+  year: 1957,
   color: "red",
   passengers: 2,
   convertible: false,
   mileage: 1021
+}
+
+const dog = {
+  name: "Fido",
+  weight: 20.2,
+  age: 4,
+  breed: "mixed",
+  activity: "fetch balls"
 }
