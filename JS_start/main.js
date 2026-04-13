38b3b734a3b56c6835fb0cedca8bc85fa7d1e57a
viktorKorneev <korneevviktor90@yandex.ru>
2026-04-13 17:00:33 +0300

    step-43

--- JS_start/main.js
+++ JS_start/main.js
@@ -183,52 +183,57 @@
 //   "Bubble Solution #" + mostCostEffective + " is the most cost effective",
 // );
 
-const chevy = {
-  make: "Chevy",
-  model: "Bel Air",
-  year: 1957,
-  color: "red",
-  passengers: 2,
-  convertible: false,
-  mileage: 1021,
-};
-
-const cadi = {
-  make: "GM",
-  model: "Cadillac",
-  year: 1955,
-  color: "tan",
-  passengers: 5,
-  convertible: false,
-  mileage: 12892,
-};
-
-const fiat = {
-  make: "Fiat",
-  model: "500",
-  year: 1957,
-  color: "Medium Blue",
-  passengers: 2,
-  convertible: false,
-  mileage: 88000,
-};
-
-function prequal(car) {
-  if (car.year > 1955 && car.mileage < 50000) {
-    return true;
-  }
-  return false;
-}
-console.log(prequal(cadi));
-
-
-// const dog = {
-//   name: "Fido",
-//   weight: 20.2,
-//   age: 4,
-//   breed: "mixed",
-//   activity: "fetch balls",
+// const chevy = {
+//   make: "Chevy",
+//   model: "Bel Air",
+//   year: 1957,
+//   color: "red",
+//   passengers: 2,
+//   convertible: false,
+//   mileage: 1021,
 // };
+
+// const cadi = {
+//   make: "GM",
+//   model: "Cadillac",
+//   year: 1955,
+//   color: "tan",
+//   passengers: 5,
+//   convertible: false,
+//   mileage: 12892,
+// };
+
+// const fiat = {
+//   make: "Fiat",
+//   model: "500",
+//   year: 1957,
+//   color: "Medium Blue",
+//   passengers: 2,
+//   convertible: false,
+//   mileage: 88000,
+// };
+
+// function prequal(car) {
+//   if (car.year > 1955 && car.mileage < 50000) {
+//     return true;
+//   }
+//   return false;
+// }
+// console.log(prequal(chevy));
+// console.log(prequal(cadi));
+// console.log(prequal(fiat));
+
+
+const dog = {
+  name: "Fido",
+  weight: 48,
+  breed: "Mixed",
+  loves: "walks",
+};
+
+function loseWaight (dog, amount) {
+  dog.weight = dog.weight - amount;
+}
 
 // if (car.mileage < 50000) {
 //   alert("Берём!!!!");
