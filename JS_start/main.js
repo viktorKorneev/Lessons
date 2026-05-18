cc25b8a06664377ee4152ab8938997b04ee38ea6
viktorKorneev <korneevviktor90@yandex.ru>
2026-05-18 22:59:27 +0300

    step-49

--- JS_start/main.js
+++ JS_start/main.js
@@ -7,39 +7,54 @@ const model = {
   courseData: {
     title: 'Фронт с "нуля"',
     lessons: [
-      {id: 1, title: "Переменные", isDone: true},
-      {id: 2, title: "Операторы", isDone: false},
-      {id: 3, title: "Массивы", isDone: false},
-      {id: 4, title: "Функции", isDone: false},
-      {id: 5, title: "Объекты", isDone: false},
-    ]
+      { id: 1, title: "Переменные", isDone: true },
+      { id: 2, title: "Операторы", isDone: false },
+      { id: 3, title: "Массивы", isDone: false },
+      { id: 4, title: "Функции", isDone: false },
+      { id: 5, title: "Объекты", isDone: false },
+    ],
   },
-  render(){
+  render() {
     // 1. delete previos view
-    console.clear()
+    console.clear();
     // 2. Create view for current state
     console.log(this.courseData);
   },
   createLesson(lessonTitle) {
-    // 1. Change data
+    // 1. Change data immutable (создать новое состояние данных, новую версию)
     const newLesson = {
       id: 6,
       title: lessonTitle,
-      isDone: false
-    }
-    this.courseData.lessons.push(newLesson)
-    //2. Update view after change data
-    this.render()
+      isDone: false,
+    };
+    const nextStatelessons = [...this.courseData.lessons, newLesson];
+    this.setState(nextStatelessons);
   },
   deleteLesson(lessonId) {
     // 1. Change data immutable (создать новое состояние данных, новую версию)
-      const nextStatelessons = this.courseData.lessons.filter(el => el.id !== lessonId)
-    // 2. Set next data to model
-      this.courseData.lessons = nextStatelessons
-    // 3. Update view after change data
-    this.render()
+    const nextStatelessons = this.courseData.lessons.filter(
+      (el) => el.id !== lessonId,
+    );
+    this.setState(nextStatelessons);
+  },
+  updateLessonStatus(lessonId) {
+    // 1. Change data immutable (создать новое состояние данных, новую версию)
+    const nextStatelessons = [...this.courseData.lessons];
+    const lesson = nextStatelessons.find((l) => l.id === lessonId);
+    lesson.isDone = !lessonId.isDone;
+    this.setState(nextStatelessons);
+  },
+  setState(nextStatelessons) {
+    if (this.courseData.lessons !== nextStatelessons) {
+      // 1. Set next data to model
+      this.courseData.lessons = nextStatelessons;
+      // 2. Update view after change data
+      this.render();
+    } else {
+      console.error("Data not changed!")
     }
-}
+  },
+};
 
-//initialization: 
-model.render()
+//initialization:
+model.render();
